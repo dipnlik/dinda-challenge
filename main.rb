dfecfdf6a788bb418f976666b7deb2fa4dfe3ea5
dipnlik <dipnlik@gmail.com>
2018-02-20 21:29:02 -0300

    add pagination

--- main.rb
+++ main.rb
@@ -1,21 +1,29 @@
 require 'net/https'
 require 'json'
 
-response = Net::HTTP.get_response URI("https://api.github.com/repos/Dinda-com-br/braspag-rest/commits")
-abort 'Error retrieving history' unless response.is_a? Net::HTTPSuccess
+commit_history = []
+page = 1
 
-history = JSON.parse response.body
+loop do
+  response = Net::HTTP.get_response URI("https://api.github.com/repos/Dinda-com-br/braspag-rest/commits?page=#{page}")
+  abort 'Error retrieving history' unless response.is_a? Net::HTTPSuccess
+
+  history_page = JSON.parse(response.body)
+  break if history_page.empty?
+  commit_history += history_page
+
+  page += 1
+end
 
 commits_per_author = {}
 
-history.each do |item|
-    author = item['commit']['author']
-    author_key = author['email']
-    author_info = { name: author['name'], email: author['email'], commit_count: 0 }
+commit_history.each do |item|
+  author = item['commit']['author']
+  author_key = author['email']
+  author_info = { name: author['name'], email: author['email'], commit_count: 0 }
 
-    commits_per_author[author_key] ||= author_info
-    commits_per_author[author_key][:commit_count] += 1
+  commits_per_author[author_key] ||= author_info
+  commits_per_author[author_key][:commit_count] += 1
 end
 
 p commits_per_author
-@commits_per_author = commits_per_author
